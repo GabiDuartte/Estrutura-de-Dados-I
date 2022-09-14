611ace61f835adec3c797bbac1e0acfb91d96441
GabiDuartte <93677092+GabiDuartte@users.noreply.github.com>
2022-09-14 20:42:40 -0300

    Update main.cpp

--- Cap2/q2.5/main.cpp
+++ Cap2/q2.5/main.cpp
@@ -6,6 +6,6 @@ int main(){
     char exp[100];
     
     printf("Informe expressao: ");
-    scanf("%99", exp);
+    scanf("%99[^\n]", exp);
     printf("\nExpresssao: %s\n Retorno: %d", exp, expressao(exp));
 }
