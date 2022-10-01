94f867e742c2fd004ccbf75db3d080b2a87760b2
Gabriela Albornoz <93677092+GabiDuartte@users.noreply.github.com>
2022-10-01 17:16:59 -0300

    Create 4-3.cpp

---
+++ cap4/4-3.cpp
@@ -0,0 +1,18 @@
+int main(){
+    char s[256];
+    Fila F = criarFila(256);
+    Pilha P = criarpilha(256);
+    printf("Frase: ");
+    gets(s);
+    for(int i = 0; s[i]; i++)
+    if(isalpha(s[i])){
+        enfileira(s[i], F);
+        empilha(s[i],P);
+    }
+    while(!vaziaf(F) && desenfileira(F)==desempilha(P));
+    if(vaziaf(F)) puts("A frase e palindroma");
+    else puts("A frase nao e palindroma");
+    destroif(&F);
+    destroip(&P);
+    return 0;
+};
