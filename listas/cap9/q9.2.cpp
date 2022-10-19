c357b05201cb4babb697eacb7445e509854c9cf7
Gabriela Albornoz <93677092+GabiDuartte@users.noreply.github.com>
2022-10-19 18:17:50 -0300

    Create q9.2.cpp

---
+++ listas/cap9/q9.2.cpp
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#define fmt "%d"
+
+using namespace std;
+
+typedef char Item;
+typedef struct no{
+    Item item;
+    struct no *prox;
+} *Lista;
+
+Lista inclue(Item x, Lista p){
+    Lista n = (Lista)malloc(sizeof(struct no));
+    n->item = x;
+    n->prox = p;
+    return n;
+}
+
+void exibe(Lista L){
+    while(L != NULL){
+        printf(fmt, L->item);
+        L = L->prox;
+    }
+}
+
+void anexa(Lista *A, Lista B){
+    if(B == NULL) return;
+    while(*A != NULL) A = &(*A)->prox;
+    *A = B;
+}
+
+void destroi(Lista *L){
+    while(*L != NULL){
+        Lista n = *L;
+        *L = n->prox;
+        free(n);
+    }
+}
+
+void ultimo(Lista L){
+    while(L != NULL){
+        if(L->prox == NULL){
+            printf(fmt, L->item);
+        }
+        L = L->prox;
+    }
+}
+
+int main(){
+    Lista l;
+    l = inclue("c", NULL);
+    l = inclue("b", l);
+    l = inclue("a", l);
+
+    exibe(l);
+    printf("\nUltimo elemento: ");
+    ultimo(l);
+}
